39eb03799ed6bd1ce0284556f641e8066986f355
Rahib20 <mrrahman10@outlook.com>
2025-07-28 08:32:40 +0100

    test

--- Scema.py
+++ Scema.py
@@ -171,11 +171,10 @@ def sample(property_name):
             return faker.ipv4()
         case "job":
             return faker.job()
-        case "country code":
-            return faker.country_code()
         case "number":
             return faker.phone_number()
-
+        case "email":
+            return faker.email()
 
     return None
 
